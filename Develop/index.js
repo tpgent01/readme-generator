905408386326b500f165306ddbe522f3d006d83b
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-14 13:03:36 -0600

    added all of the user prompts

--- Develop/index.js
+++ Develop/index.js
@@ -44,6 +44,72 @@ const questions = () => {
                 }
             }
         },
+        {
+            type: 'confirm',
+            name: 'confirmDescription',
+            message: 'Would you like to enter some ifnormation about your project for a "Description" section?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide some information about your project:',
+            when: ({ confirmDescription }) => confirmDescription
+        },
+        {
+            type: 'confirm',
+            name: 'confirmInstall',
+            message: 'Would you like to provide installation instructions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'installtion',
+            message: 'Provide installation instructions:',
+            when: ({ confirmInstall }) => confirmInstall
+        },
+        {
+            type: 'confirm',
+            name: 'confirmUsage',
+            message: 'Would you like to provide usage information?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide usage information:',
+            when: ({ confirmUsage }) => confirmUsage
+        },
+        {
+            type: 'checkbox',
+            name: 'license',
+            message: 'Please choose a license for your project:',
+            choices: ['MIT', 'BSD', 'GPL']
+        },
+        {
+            type: 'confirm',
+            name: 'confirmContribution',
+            message: 'Would you like to provide contributing guidlines?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Provide contribution guidlines:',
+            when: ({ confirmContribution }) => confirmContribution
+        },
+        {
+            input: 'confirm',
+            name: 'confirmTest',
+            message: 'Would you like to provide test instructions?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Provide test instructions:',
+            when: ({ confirmTest }) => confirmTest
+        }
     ])
 };
 
