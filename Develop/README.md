1597aee48ef85c67ef957dfe51b8bc83f29920d6
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-14 13:53:29 -0600

    ran test

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+# README Generator
+![GitHub license](https://img.shields.io/badge/license--yellowgreen.svg)
+## 🌐 Github URL
+[tpgent01](https://github.com/tpgent01/)
+## 💬 Description 
+This is a README generator
+## 📓 Table of Contents 
+* [Installations](#dependencies)
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributors](#contributors)
+* [Test](#test)
+## 🔌 Installations (Dependencies)
+To install dependencies, run these commands:
+```
+undefined
+```
+## 🎨 Usage
+insert usage here
+## 📛 License
+      Copyright © . All rights reserved. 
+      
+      Licensed under the  license.
+## ✏️ Contributors
+insert contribute here
+Contact me at tpgent01@outlook.com
+## 📊 Tests
+To run tests, run these commands:
+```
+test here
+```
--- Develop/index.js
+++ Develop/index.js
@@ -5,115 +5,114 @@ const path = require('path');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = projectData => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project? (Required)',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your project title!');
-                    return false;
-                }
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title!');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'What is your GitHub username? (Required)',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your GitHub username!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'What is your email address? (Required)',
-            validate: emailInput => {
-                if (emailInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your email address!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'confirm',
-            name: 'confirmDescription',
-            message: 'Would you like to enter some ifnormation about your project for a "Description" section?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide some information about your project:',
-            when: ({ confirmDescription }) => confirmDescription
-        },
-        {
-            type: 'confirm',
-            name: 'confirmInstall',
-            message: 'Would you like to provide installation instructions?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'installtion',
-            message: 'Provide installation instructions:',
-            when: ({ confirmInstall }) => confirmInstall
-        },
-        {
-            type: 'confirm',
-            name: 'confirmUsage',
-            message: 'Would you like to provide usage information?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Provide usage information:',
-            when: ({ confirmUsage }) => confirmUsage
-        },
-        {
-            type: 'checkbox',
-            name: 'license',
-            message: 'Please choose a license for your project:',
-            choices: ['MIT', 'BSD', 'GPL', 'None']
-        },
-        {
-            type: 'confirm',
-            name: 'confirmContribution',
-            message: 'Would you like to provide contributing guidlines?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Provide contribution guidlines:',
-            when: ({ confirmContribution }) => confirmContribution
-        },
-        {
-            input: 'confirm',
-            name: 'confirmTest',
-            message: 'Would you like to provide test instructions?',
-            default: true
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Provide test instructions:',
-            when: ({ confirmTest }) => confirmTest
         }
-    ]);
-};
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username? (Required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'confirm',
+        name: 'confirmDescription',
+        message: 'Would you like to enter some information about your project for a "Description" section?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide some information about your project:',
+        when: ({ confirmDescription }) => confirmDescription
+    },
+    {
+        type: 'confirm',
+        name: 'confirmInstall',
+        message: 'Would you like to provide installation instructions?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'installtion',
+        message: 'Provide installation instructions:',
+        when: ({ confirmInstall }) => confirmInstall
+    },
+    {
+        type: 'confirm',
+        name: 'confirmUsage',
+        message: 'Would you like to provide usage information?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:',
+        when: ({ confirmUsage }) => confirmUsage
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please choose a license for your project:',
+        choices: ['MIT', 'BSD', 'GPL', 'None']
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContribution',
+        message: 'Would you like to provide contributing guidlines?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Provide contribution guidlines:',
+        when: ({ confirmContribution }) => confirmContribution
+    },
+    {
+        input: 'confirm',
+        name: 'confirmTest',
+        message: 'Would you like to provide test instructions?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Provide test instructions:',
+        when: ({ confirmTest }) => confirmTest
+    }
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -124,7 +123,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions)
     .then((inquirerAnswers) => {
-        console.log("generating...Please wait...");
+        console.log("generating...please wait...");
         writeToFile("README.md", generateMarkdown({ ...inquirerAnswers }));
     })
 }
